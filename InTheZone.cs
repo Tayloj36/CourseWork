12de0cf00595cd65d33026f0d24ce4801597ef20
Joshua Taylor <Tayloj36@gmail.com>
2016-11-15 12:43:18 +0000

    Add files via upload
    Whoops, I've been a huge fool, Turns out you have to define the "Player" in the trigger as a public gameobject for the thing to work. Anyhow. to make this work, follow the steps to create the desired animation, then apply the respective animation controller on the trigger. Simplicity itself. Anyway, to make up for my lack of activity I will be creating a new component for you guys,

---
+++ InTheZone.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InTheZone : MonoBehaviour {
+
+	public Animator anim;
+	public GameObject player; 
+	public int ZonedIn = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		ZonedIn -= 1;
+
+		if (ZonedIn <= 0) {
+
+			ZonedIn = 0;
+		
+				
+		}
+		if (ZonedIn >= 1) {
+			
+			anim.SetInteger ("In", 1);
+	
+		}
+		else if (ZonedIn <= 0) {
+			anim.SetInteger ("In", 0);
+		}
+	}
+
+	public void OnTriggerStay(Collider other){
+
+		if (other.name == "player") {
+
+			ZonedIn = 10;
+
+			
+		}
+	
+
+}
+}
+
+
